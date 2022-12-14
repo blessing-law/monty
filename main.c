d37b8eda9d0eca3ef33641595f58913edcbf1030
blessing-law <blessing.lawrence77@gmail.com>
2022-12-14 10:18:15 -0800

    main.c

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #include "monty.h"
-#include <stdio.h>
-#define _GNU_SOURCE
-#include <stdlib.h>
+bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
-* main - function for monty code interpreter
-* @argc: argument count
-* @argv: argument value
-*
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
 * Return: 0 on success
 */
+
 int main(int argc, char *argv[])
 {
 	char *content;
@@ -24,10 +22,14 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
+
 	bus.file = file;
+
 	if (!file)
 	{
+
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
@@ -37,6 +39,7 @@ int main(int argc, char *argv[])
 		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
+
 		if (read_line > 0)
 		{
 			execute(content, &stack, counter, file);
@@ -45,5 +48,6 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
+
 return (0);
 }
